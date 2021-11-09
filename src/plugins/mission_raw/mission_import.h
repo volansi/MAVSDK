409ac96ce91d47f362fa2f3d07ee7cb9f42a4379
Kenneth Thompson <ken@flyvoly.com>
2021-11-09 12:41:50 -0800

    mission_raw: Add support for geofence and rally points

--- src/plugins/mission_raw/mission_import.h
+++ src/plugins/mission_raw/mission_import.h
@@ -17,10 +17,18 @@ private:
     static bool check_overall_version(const Json::Value& root);
     static std::optional<std::vector<MissionRaw::MissionItem>>
     import_mission(const Json::Value& root);
+    static std::optional<std::vector<MissionRaw::MissionItem>>
+    import_geofence(const Json::Value& root);
+    static std::optional<std::vector<MissionRaw::MissionItem>>
+    import_rally_points(const Json::Value& root);
     static std::optional<MissionRaw::MissionItem>
     import_simple_mission_item(const Json::Value& json_item);
     static std::optional<std::vector<MissionRaw::MissionItem>>
     import_complex_mission_items(const Json::Value& json_item);
+    static std::vector<MissionRaw::MissionItem>
+    import_polygon_geofences(const Json::Value& json_item);
+    static std::vector<MissionRaw::MissionItem>
+    import_circular_geofences(const Json::Value& json_item);
 };
 
 } // namespace mavsdk
